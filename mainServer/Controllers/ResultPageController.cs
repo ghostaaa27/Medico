043d2d744de7a4dbc16be48b27627e5ae2f1322d
Abdullah Al-Amin <78086376+ghostaaa27@users.noreply.github.com>
2022-05-31 23:19:10 +0600

    a

--- mainServer/Controllers/ResultPageController.cs
+++ mainServer/Controllers/ResultPageController.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Mvc;
 using Server2;
 
@@ -5,7 +6,8 @@ namespace mainServer.Controllers{
 
     public class ResultPageController : BaseController
     {
-        public IActionResult Index(){
+        public IActionResult Index(string disease){
+            Console.WriteLine(disease);
             return View("~/Views/result_page.cshtml");
         }
     }
