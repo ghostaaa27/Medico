2883fda94230d604753f18f6b946d9bd50f5831e
Abdullah Al-Amin <78086376+ghostaaa27@users.noreply.github.com>
2022-06-11 00:58:45 +0600

    aa

--- mainServer/Controllers/ResultPageController.cs
+++ mainServer/Controllers/ResultPageController.cs
@@ -4,11 +4,20 @@ using Server2;
 
 namespace mainServer.Controllers{
 
+    public class Result_Model{
+        public string disease {get; set;}
+        public string accuracy {get; set;}
+    }
+
     public class ResultPageController : BaseController
     {
-        public IActionResult Index(string disease){
-            Console.WriteLine(disease);
-            return View("~/Views/result_page.cshtml");
+        public IActionResult Index(string disease, string accuracy){
+            // Console.WriteLine(disease + accuracy);
+
+            Result_Model rm = new Result_Model();
+            rm.accuracy = accuracy;
+            rm.disease = disease;
+            return View("~/Views/result_page.cshtml", rm);
         }
     }
 
