3b28423bf45166a1c7173331ac1fd1a23537f832
Abdullah Al-Amin <78086376+ghostaaa27@users.noreply.github.com>
2022-08-30 16:22:03 +0600

    a

--- mainServer/Controllers/DoctorDetailsController.cs
+++ mainServer/Controllers/DoctorDetailsController.cs
@@ -1,20 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using MediShare.Controllers;
 using Microsoft.AspNetCore.Mvc;
+using Server;
 using Server2;
 
 namespace mainServer.Controllers{
 
+
+
+    public class DoctorDetails{
+        public string email{get;set;}
+        public string name{get;set;}
+        public string institute{get;set;}
+        public string address{get;set;}
+    }
     public class DoctorDetailsController: BaseController
     {
-        public ActionResult Index(){
-            string s_email = GetUserEMAIL();
+        // public ActionResult Index(){
+        //     string s_email = GetUserEMAIL();
 
 
-            SessionCheckerModel sessionCheckerModel = new SessionCheckerModel();
-            sessionCheckerModel.email = s_email;
+        //     SessionCheckerModel sessionCheckerModel = new SessionCheckerModel();
+        //     sessionCheckerModel.email = s_email;
 
         
-            return View("~/Views/doctor_details_page.cshtml", sessionCheckerModel);
+        //     return View("~/Views/doctor_details_page.cshtml", sessionCheckerModel);
+        // }
+
+        public async Task<ActionResult<List<DoctorDetails>>> Index(int id, string name, string institute, string address){
+          
+            String s_email = GetUserEMAIL();
+
+            DoctorDetails dd = new DoctorDetails();
+            dd.name = name;
+            dd.institute = institute;
+            dd.address = address;
+            dd.email = s_email;
+
+            return View("~/Views/doctor_details_page.cshtml", dd);
         }
     }
 }
--- mainServer/Controllers/DoctorListController.cs
+++ mainServer/Controllers/DoctorListController.cs
@@ -1,8 +1,21 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using MediShare.Controllers;
 using Microsoft.AspNetCore.Mvc;
+using Server;
 using Server2;
 
 namespace mainServer.Controllers{
+
+
+
+    public class DoctorList{
+        public int id{get;set;}
+        public string name{get;set;}
+        public string institute{get;set;}
+        public string address{get;set;}
+        public string speciality{get; set;}
+    }
     
     public class DoctorListController : BaseController
     {
@@ -18,6 +31,16 @@ namespace mainServer.Controllers{
 
         
             return View("~/Views/doctor_list_page.cshtml", sessionCheckerModel);
+        }
+
+        public async Task<ActionResult<List<DoctorList>>> Dlist(){
+            var res = await DAL.ExecuteReaderAsync<DoctorList>(
+                    @"SELECT * FROM doctordetails",
+                    new string[,]{
+                    }
+                );
+
+            return res;
         }
     }
 }
